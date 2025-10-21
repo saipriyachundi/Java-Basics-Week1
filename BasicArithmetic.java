9ec462bf444d316fdd21b6105a29321a3a5befa1
saipriya0730-ship-it <saipriya0730@gmail.com>
2025-10-21 07:43:42 -0700

    Week 1 Java assignments - HelloWorld, VariablesPractice, BasicArithmetic

---
+++ BasicArithmetic.java
@@ -0,0 +1,12 @@
+public class BasicArithmetic {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 6;
+
+        System.out.println("Addition: " + (num1 + num2));
+        System.out.println("Subtraction: " + (num1 - num2));
+        System.out.println("Multiplication: " + (num1 * num2));
+        System.out.println("Division: " + (num1 / num2));
+        System.out.println("Modulus: " + (num1 % num2));
+    }
+}
---
+++ VariablesPractice.java
@@ -0,0 +1,11 @@
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int age = 25;
+        double salary = 65000.50;
+        String name = "Saipriya";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: $" + salary);
+    }
+}
